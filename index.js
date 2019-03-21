f2fd4cfce43b711f615a9128c7a57a7e15962af8
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-21 16:59:33 +0700

    environment variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello NodeJs');
@@ -10,4 +10,4 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
