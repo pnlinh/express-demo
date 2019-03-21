d80ab4dc51384057a81b164651fd99156703e48b
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-21 17:34:35 +0700

    route parameter

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
